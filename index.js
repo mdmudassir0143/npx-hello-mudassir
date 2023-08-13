a6edb91cb9b1edf022f7a58e84fcc12a195783ea
Mohammad Mudassir <mdmudassir0143@gmail.com>
2023-08-13 22:26:10 +0530

    initials

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// Function to log your details in the terminal
+function logDetails() {
+    const message = "Hey there! I'm Mohammad Mudassir, Blockchain Developer and Researcher";
+    const twitterLink = "https://twitter.com/mdmudassir0143";
+    const linkedinLink = "https://www.linkedin.com/in/mohammad-mudassir-b788a41ab/";
+    const wesbiteLink = "https://mdmudassir.live";
+  
+    // Create a colorful box using ANSI escape codes
+    const colorfulBox = `  \x1b[38;5;51m+---------------------------------------------------------------+
+    |   \x1b[38;5;105m${message}\x1b[38;5;51m    |
+    +---------------------------------------------------------------+
+    | \x1b[38;5;93mTwitter:\x1b[0m \x1b[38;5;39m${twitterLink}                     \x1b[38;5;51m|
+    | \x1b[38;5;93mLinkedIn:\x1b[0m \x1b[38;5;39m${linkedinLink}                 \x1b[38;5;51m|
+    | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${wesbiteLink}                      \x1b[38;5;51m|
+    +----------------------------------------------------------------+\x1b[0m`;
+  
+    // Log the colorful box in the terminal
+    console.log(colorfulBox);
+  }
+  // Call the function to log your details
+  logDetails();
